623d87151d4be58c80c722b40f1d17058db4293e
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2024-11-19 18:43:12 -0300

    support for github-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://prom97.github.io/ffxiii-react', // Link page
 })
